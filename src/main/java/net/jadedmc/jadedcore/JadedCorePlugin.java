5fe36c4bf768d00d65302193ad8524d0de1d4245
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-01-29 23:30:47 -0500

    Initial commit

---
+++ src/main/java/net/jadedmc/jadedcore/JadedCorePlugin.java
@@ -0,0 +1,17 @@
+package net.jadedmc.jadedcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JadedCorePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
