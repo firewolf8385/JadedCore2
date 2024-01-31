2e0409646d05b9db8ac32df782cd5127c3ee00d2
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-01-30 21:49:53 -0500

    MySQL Changes

--- src/main/java/net/jadedmc/jadedcore/databases/MySQL.java
+++ src/main/java/net/jadedmc/jadedcore/databases/MySQL.java
@@ -28,7 +28,6 @@ import net.jadedmc.jadedcore.JadedCorePlugin;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -47,27 +46,13 @@ public class MySQL {
      * Loads the MySQL database connection info.
      * @param plugin Instance of the plugin.
      */
-    public MySQL(JadedCorePlugin plugin) {
+    public MySQL(final JadedCorePlugin plugin) {
         this.plugin = plugin;
         host = plugin.settingsManager().getConfig().getString("MySQL.host");
         database = plugin.settingsManager().getConfig().getString("MySQL.database");
         username = plugin.settingsManager().getConfig().getString("MySQL.username");
         password = plugin.settingsManager().getConfig().getString("MySQL.password");
         port = plugin.settingsManager().getConfig().getInt("MySQL.port");
-    }
-
-    /**
-     * Close a connection.
-     */
-    public void closeConnection() {
-        if(isConnected()) {
-            try {
-                connection.close();
-            }
-            catch(SQLException exception) {
-                exception.printStackTrace();
-            }
-        }
     }
 
     /**
@@ -112,48 +97,6 @@ public class MySQL {
                     exception.printStackTrace();
                 }
             }, 504000, 504000);
-
-            {
-                PreparedStatement api_keys = connection.prepareStatement("CREATE TABLE IF NOT EXISTS api_keys (" +
-                        "uuid VARCHAR(36)," +
-                        "apiKey VARCHAR(36)," +
-                        "PRIMARY KEY(uuid, apiKey)" +
-                        ");");
-                api_keys.execute();
-            }
-
-            {
-                PreparedStatement staff_settings = connection.prepareStatement("CREATE TABLE IF NOT EXISTS staff_settings (" +
-                        "uuid VARCHAR(36)," +
-                        "vanish BOOLEAN DEFAULT FALSE," +
-                        "commandSpy BOOLEAN DEFAULT FALSE," +
-                        "PRIMARY KEY(uuid)" +
-                        ");");
-                staff_settings.execute();
-            }
-
-            {
-                PreparedStatement achievements_list = connection.prepareStatement("CREATE TABLE IF NOT EXISTS achievements_list (" +
-                        "id VARCHAR(36)," +
-                        "mode VARCHAR(36)," +
-                        "name VARCHAR(36)," +
-                        "description VARCHAR(128)," +
-                        "achievementPoints INT DEFAULT 0, " +
-                        "rewards VARCHAR(256)," +
-                        "PRIMARY KEY(id)" +
-                        ");");
-                achievements_list.execute();
-            }
-
-            {
-                PreparedStatement player_achievements = connection.prepareStatement("CREATE TABLE IF NOT EXISTS player_achievements (" +
-                        "uuid VARCHAR(36)," +
-                        "achievementID VARCHAR(36), " +
-                        "time TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
-                        "PRIMARY KEY(uuid,achievementID)" +
-                        ");");
-                player_achievements.execute();
-            }
         }
         catch(SQLException | ClassNotFoundException exception) {
             exception.printStackTrace();
