db406129ddef7853c044cc38b3ab6f5f437d758f
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-02-03 04:58:24 -0500

    Hide FindMCServer from the vote list, since that will take some time

--- src/main/java/net/jadedmc/jadedcore/commands/VoteCMD.java
+++ src/main/java/net/jadedmc/jadedcore/commands/VoteCMD.java
@@ -39,9 +39,9 @@ public class VoteCMD extends AbstractCommand {
         ChatUtils.chat(sender, "");
         ChatUtils.chat(sender, ChatUtils.centerText("&a&lVote Links"));
         ChatUtils.chat(sender, "");
-        ChatUtils.chat(sender, "<hover:show_text:'<green>Click to Visit'><dark_gray>  » <green><click:open_url:'https://findmcserver.com/server/s3zoZ02yd0'>Vote Site 1</click></hover>");
-        ChatUtils.chat(sender, "<hover:show_text:'<green>Click to Visit'><dark_gray>  » <green><click:open_url:'https://www.planetminecraft.com/server/elytrapvp-4338121/vote/'>Vote Site 2</click></hover>");
-        ChatUtils.chat(sender, "<hover:show_text:'<green>Click to Visit'><dark_gray>  » <green><click:open_url:'https://minecraftservers.org/vote/569103'>Vote Site 3</click></hover>");
+        //ChatUtils.chat(sender, "<hover:show_text:'<green>Click to Visit'><dark_gray>  » <green><click:open_url:'https://findmcserver.com/server/s3zoZ02yd0'>Vote Site 1</click></hover>");
+        ChatUtils.chat(sender, "<hover:show_text:'<green>Click to Visit'><dark_gray>  » <green><click:open_url:'https://www.planetminecraft.com/server/elytrapvp-4338121/vote/'>Vote Site 1</click></hover>");
+        ChatUtils.chat(sender, "<hover:show_text:'<green>Click to Visit'><dark_gray>  » <green><click:open_url:'https://minecraftservers.org/vote/569103'>Vote Site 2</click></hover>");
         ChatUtils.chat(sender, "&a▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
     }
 }
