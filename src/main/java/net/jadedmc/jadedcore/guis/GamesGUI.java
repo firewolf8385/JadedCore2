39b9828f5dd7cee65c2ab2b77eefec76540c8e8b
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-04-30 05:53:10 -0400

    Update Games GUI

--- src/main/java/net/jadedmc/jadedcore/guis/GamesGUI.java
+++ src/main/java/net/jadedmc/jadedcore/guis/GamesGUI.java
@@ -27,9 +27,11 @@ package net.jadedmc.jadedcore.guis;
 import com.cryptomorin.xseries.XMaterial;
 import net.jadedmc.jadedcore.JadedAPI;
 import net.jadedmc.jadedcore.games.Game;
+import net.jadedmc.jadedcore.minigames.Minigame;
 import net.jadedmc.jadedutils.chat.ChatUtils;
 import net.jadedmc.jadedutils.gui.CustomGUI;
 import net.jadedmc.jadedutils.items.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.ChatPaginator;
@@ -38,64 +40,60 @@ public class GamesGUI extends CustomGUI {
 
     public GamesGUI() {
         super(54, "Games");
-
         addFiller(0,1,2,3,4,5,6,7,8,45,46,47,48,49,50,51,52,53);
 
-        addGame(20, Game.CACTUS_RUSH);
-        addGame(21, Game.ELYTRAPVP);
-        addGame(23, Game.TURFWARS);
-        //addGame(24, Game.HOUSING);
-        addGame(31, Game.LOBBY);
+        ItemBuilder cactusRush = new ItemBuilder(Material.CACTUS)
+                .setDisplayName("<green><bold>Cactus Rush")
+                .addLore("<dark_gray>Competitive")
+                .addLore("")
+                .addLore("<gray>Team-Based Cactus")
+                .addLore("<gray>Fighting Minigame.")
+                .addLore("")
+                .addLore("<green>▸ Click to Connect")
+                .addLore("<gray>Join " + JadedAPI.getInstanceMonitor().getPlayerCount(Minigame.CACTUS_RUSH) + " others playing!");
+        setItem(20, cactusRush.build(), (p,a) -> JadedAPI.sendBungeecordMessage(p, "BungeeCord", "Connect", "cactusrush"));
 
-        ItemStack comingSoon = new ItemBuilder(XMaterial.GRAY_DYE)
-                .setDisplayName("<red><bold>Coming Soon")
-                .build();
-
-        //setItem(22, comingSoon);
-        //setItem(23, comingSoon);
-        setItem(24, comingSoon);
-
-        ItemStack duels = new ItemBuilder(XMaterial.IRON_SWORD)
+        ItemBuilder duels = new ItemBuilder(Material.IRON_SWORD)
                 .setDisplayName("<green><bold>Duels")
+                .addLore("<dark_gray>Competitive")
                 .addLore("")
-                .addLore(ChatPaginator.wordWrap(Game.DUELS.getDescription(), 25), "<gray>")
-                .addLore("")
-                .addLore("<green>▸ Click to Connect")
-                .addLore(ChatUtils.parsePlaceholders("<gray>Join %math_0_{bungee_modernduels}+{bungee_legacyduels}+{bungee_tournament}% others playing!"))
-                .addFlag(ItemFlag.HIDE_ATTRIBUTES)
-                .build();
-        setItem(22, duels, (p, a) -> new DuelsGUI().open(p));
-    }
-
-    private void addGame(int slot, Game game) {
-        setItem(slot, getGameIcon(game), (p, a) -> JadedAPI.sendBungeecordMessage(p, "BungeeCord", "Connect", game.getServer()));
-    }
-
-    private ItemStack getGameIcon(Game game) {
-        XMaterial material = game.getIconMaterial();
-
-        if(JadedAPI.getCurrentInstance().getMajorVersion() < 9 && material == XMaterial.ELYTRA) {
-            material = XMaterial.FEATHER;
-        }
-
-        if(game == Game.LOBBY) {
-            return new ItemBuilder(material)
-                    .setDisplayName("<green><bold>" + game.getName())
-                    .addLore("")
-                    .addLore("<green>▸ Click to Connect")
-                    .addLore(ChatUtils.parsePlaceholders("<gray>Join %bungee_" + game.getServer() + "% others playing!"))
-                    .build();
-        }
-
-        ItemBuilder builder = new ItemBuilder(material)
-                .setDisplayName("<green><bold>" + game.getName())
-                .addLore("<dark_gray>" + game.getType())
-                .addLore("")
-                .addLore(ChatPaginator.wordWrap(game.getDescription(), 25), "&7")
+                .addLore("<gray>Arena-based pvp duels")
+                .addLore("<gray>with a variety of kits!")
                 .addLore("")
                 .addLore("<green>▸ Click to Connect")
-                .addLore(ChatUtils.parsePlaceholders("<gray>Join %bungee_" + game.getServer() + "% others playing!"))
-                .addFlag(ItemFlag.HIDE_ATTRIBUTES);
-        return builder.build();
+                .addLore("<gray>Join " + JadedAPI.getInstanceMonitor().getPlayerCount(Minigame.DUELS_MODERN) + " others playing!");
+        setItem(22, duels.build(), (p,a) -> JadedAPI.sendToLobby(p, Minigame.DUELS_MODERN));
+
+        ItemBuilder elytraPvP = new ItemBuilder(Material.ELYTRA)
+                .setDisplayName("<green><bold>ElytraPvP")
+                .addLore("<dark_gray>Persistent Game")
+                .addLore("")
+                .addLore("<gray>Action-Packed pvp in the")
+                .addLore("<gray>air using bows!")
+                .addLore("")
+                .addLore("<green>▸ Click to Connect")
+                .addLore("<gray>Join " + JadedAPI.getInstanceMonitor().getPlayerCount(Minigame.ELYTRAPVP) + " others playing!");
+        setItem(24, elytraPvP.build(), (p,a) -> JadedAPI.sendBungeecordMessage(p, "BungeeCord", "Connect", "elytrapvp"));
+
+        ItemBuilder lobby = new ItemBuilder(Material.BOOKSHELF)
+                .setDisplayName("<green><bold>Main Lobby")
+                .addLore("")
+                .addLore("<green>▸ Click to Connect")
+                .addLore("<gray>Join " + JadedAPI.getInstanceMonitor().getPlayerCount(Minigame.HUB) + " others playing!");
+        setItem(30, lobby.build(), (p,a) -> JadedAPI.sendToLobby(p, Minigame.HUB));
+
+        ItemBuilder limbo = new ItemBuilder(Material.BEDROCK)
+                .setDisplayName("<green><bold>Limbo")
+                .addLore("")
+                .addLore("<green>▸ Click to Connect")
+                .addLore("<gray>Join " + JadedAPI.getInstanceMonitor().getPlayerCount(Minigame.LIMBO) + " others playing!");
+        setItem(31, limbo.build(), (p,a) -> JadedAPI.sendBungeecordMessage(p, "BungeeCord", "Connect", "limbo"));
+
+        ItemBuilder tournaments = new ItemBuilder(Material.GOLD_INGOT)
+                .setDisplayName("<green><bold>Tournament Lobby")
+                .addLore("")
+                .addLore("<green>▸ Click to Connect")
+                .addLore("<gray>Join " + JadedAPI.getInstanceMonitor().getPlayerCount(Minigame.TOURNAMENTS_MODERN) + " others playing!");
+        setItem(32, tournaments.build(), (p,a) -> JadedAPI.sendToLobby(p, Minigame.TOURNAMENTS_MODERN));
     }
 }
